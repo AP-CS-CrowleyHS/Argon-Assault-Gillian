401030584ddae3dc041b196a7f1638fcd88cbabd
Gillian <gillibeanbuckelew@gmail.com>
2018-10-04 16:07:31 -0500

    Oct 4 End of 8th

---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+
+        float rawNewX = transform.localPosition.x + xOffset;
+        float clampedX = Mathf.Clamp(rawNewX, -xRange, xRange);
+
+        transform.localPosition = new Vector3(clampedX, transform.localPosition.y, transform.localPosition.z);
+	}
+}
