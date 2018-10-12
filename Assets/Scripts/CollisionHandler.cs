20fe77bdad342324c80b8470e268399ac2ac2a0f
Gillian <gillibeanbuckelew@gmail.com>
2018-10-12 15:56:51 -0500

    Oct 12

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour {
+    [SerializeField] float levelLoadDay = 1f;
+    private void OnTriggerEnter(Collider other)
+    {
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        SendMessage("OnPlayerDeath");
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour {
-    [Tooltip("In ms^-1")] [SerializeField] float Speed = 5f;
+public class PlayerController : MonoBehaviour {
+    [Header("General")]
+    [Tooltip("In ms^-1")] [SerializeField] float controlSpeed = 50f;
     [Tooltip("In m")] [SerializeField] float xRange = 7f;
     [Tooltip("In m")] [SerializeField] float yRange = 4.5f;
-
+    [Header("Screen Position")]
     [SerializeField] float posPitchFactor = -5f;
-    [SerializeField] float controlPitchFactor = -20f;
     [SerializeField] float posYawFactor = 5f;
+    [Header("Control- Throw")]
+    [SerializeField] float controlPitchFactor = -20f;
     [SerializeField] float controlRollFactor = -20f;
 
     float xThrow; float yThrow;
-    // Use this for initialization
-    void Start () {
-		
-	}
-    private void OnTriggerEnter(Collider other)
-    {
-        print("Player triggered something");
-    }
-
+    bool controlsEnabled = true;
+    
     // Update is called once per frame
     void Update ()
     {
-        ProcessTranslation();
-        ProcessRotation();
+        if (controlsEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
+        
     }
 
     private void ProcessRotation()
@@ -44,14 +43,18 @@ public class Player : MonoBehaviour {
         xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
-        float xOffset = xThrow * Speed * Time.deltaTime;
+        float xOffset = xThrow * controlSpeed * Time.deltaTime;
         float rawNewX = transform.localPosition.x + xOffset;
         float clampedX = Mathf.Clamp(rawNewX, -xRange, xRange);
 
-        float yOffset = yThrow * Speed * Time.deltaTime;
+        float yOffset = yThrow * controlSpeed * Time.deltaTime;
         float rawNewY = transform.localPosition.y + yOffset;
         float clampedY = Mathf.Clamp(rawNewY, -yRange, yRange);
 
         transform.localPosition = new Vector3(clampedX, clampedY, transform.localPosition.z);
+    }
+    private void OnPlayerDeath() //called by Collsion Handler
+    {
+        controlsEnabled = false;
     }
 }
