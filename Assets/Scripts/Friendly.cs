eed45885160e3d5dc05e996bf12934be2e06cf4d
Gillian <gillibeanbuckelew@gmail.com>
2018-10-22 16:01:16 -0500

    Completed Current Enemies Added a Friendly

---
+++ Assets/Scripts/Friendly.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Friendly : MonoBehaviour {
+    [SerializeField] GameObject DeathFX;
+    [SerializeField] Transform parent;
+    // Use this for initialization
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject Fx = Instantiate(DeathFX, transform.position, Quaternion.identity);
+        Fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider enemyCollide = gameObject.AddComponent<BoxCollider>();
+        enemyCollide.isTrigger = false;
+    }
+}
